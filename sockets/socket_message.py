65294fd952b553674a867b7edb1fc8c233d68f8a
choi woo-nyong <woonyong.kr@gmail.com>
2026-03-06 08:35:23 +0900

    채팅 룸 구현

--- sockets/socket_message.py
+++ sockets/socket_message.py
@@ -1,15 +1,24 @@
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room
 from flask import request
 
 host = "0.0.0.0"
 port = 12345
 
-
 socketio = SocketIO()
 messages = []
+
+@socketio.on('join')
+def on_join(data):
+    room = data.get('room')
+    if room:
+        join_room(room)
 
 @socketio.on('message')
 def handle_message(data):
     msg = {'text': data['text'], 'userId': data['userId'], 'nickname': data.get('nickname', '')}
+    room = data.get('room')
     messages.append(msg)
-    emit('message', msg, broadcast=True)
+    if room:
+        emit('message', msg, to=room)
+    else:
+        emit('message', msg, broadcast=True)
