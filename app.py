49e8cbca659b44250dcf9ebeed604670ffd0ec0e
JeongYeonH <kukuro1995@naver.com>
2026-03-05 22:19:11 +0900

    수정

--- app.py
+++ app.py
@@ -56,13 +56,18 @@ def home():
                            )
 
 
+
 # 카드 상세 화면 api
-@app.route('/food/<string:card_id>')
+@app.route('/food/<string:card_id>', methods=['GET'])
 def article(card_id):
-    card = db.cards.find_one({'_id': ObjectId(card_id)})
-    card['_id'] = str(card['_id'])
-    card['card_duedate'] = time.strftime('%Y-%m-%d %H:%M', time.localtime(card['card_duedate']))
-    return render_template('article.html', card=card)
+   card = db.cards.find_one({'_id': ObjectId(card_id)})
+   # print( "찾은 카드 " + card)
+   card['_id'] = str(card['_id'])
+   card['card_duedate'] = time.strftime('%Y-%m-%d %H:%M', time.localtime(card['card_duedate']))
+   # return jsonify({'result': 'success' , 'data' : card })
+   return render_template('article.html', card=card)
+
+
 
 #추가함 회원가입 api
 @app.route('/food/signin', methods=['POST'])
@@ -339,8 +344,7 @@ def show_more():
         "next_cursor": next_cursor, 
         "has_more": has_more
    })
-
-   return jsonify({'result': 'success', 'message': '아직 준비 중'})
+   # return jsonify({'result': 'success', 'message': '아직 준비 중'})
 
 
 
